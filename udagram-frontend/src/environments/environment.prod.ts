9dd587252825dbd9a579343b73e5b96527380e20
KehindeBello <adedotunomomeji@gmail.com>
2022-09-29 23:48:37 +0100

    updated apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a760903d7fddb480d85abfc9ecfcc82c-550686759.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
